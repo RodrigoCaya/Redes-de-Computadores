97472b728af3bf6d96651e4c45cbc153624a1a55
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-04-28 19:30:23 +0200

    udp y tcp basico

--- cliente.py
+++ cliente.py
@@ -1,16 +1,31 @@
 import socket as sock
 
 direccionServidor = 'localhost'
-# 49152 - 65535
-puertoServidor = 55555
 
-#inet = ipv4 | dgram = udp
-socketCliente = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+def ClienteUDP():
+    # 49152 - 65535
+    puertoServidor = 55555
+    #inet = ipv4 | dgram = udp
+    socketCliente = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+    aEnviar = input("Ingresar texto: ")
+    socketCliente.sendto(aEnviar.encode(), (direccionServidor, puertoServidor))
+    #esperar una respuesta
+    mensaje, _ = socketCliente.recvfrom(2048)
+    print(mensaje.decode())
+    socketCliente.close()
 
-aEnviar = input("Ingresar texto: ")
-socketCliente.sendto(aEnviar.encode(), (direccionServidor, puertoServidor))
 
-#esperar una respuesta
-mensaje, _ = socketCliente.recvfrom(2048)
-print(mensaje.decode())
-socketCliente.close()
+def ClienteTCP():
+    puertoServidor = 55556
+    socketCliente = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+    #esta funcion realiza el handshake
+    socketCliente.connect((direccionServidor, puertoServidor))
+    aEnviar = input('Ingrese texto: ')
+    socketCliente.send(aEnviar.encode())
+    respuesta = socketCliente.recv(2048).decode()
+    print(respuesta)
+    socketCliente.close()
+
+
+#ClienteUDP()
+#ClienteTCP()
--- servidor.py
+++ servidor.py
@@ -1,20 +1,40 @@
 import socket as sock
 
-# 49152 - 65535
-serverPort = 55555
 
-#inet = ipv4 | dgram = udp
-socketServidor = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+def ServidorUDP():
+    # 49152 - 65535
+    serverPort = 55555
+    #inet = ipv4 | dgram = udp
+    socketServidor = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+    socketServidor.bind(('', serverPort))
+    print("Servidor escuchando en puerto:", serverPort)
+    while True:
+        mensaje, direccionCliente = socketServidor.recvfrom(2048)#la cantidad de bits que recive a través del socket - tamaño del buffer
+        decodificado = mensaje.decode()
+        print("Se recibio: ", decodificado)
+        respuesta = "Respuesta: " + decodificado.upper()#dejarlo en mayuscula
+        socketServidor.sendto(respuesta.encode(),direccionCliente)
+        socketServidor.close()
 
-socketServidor.bind(('', serverPort))
+def ServidorTCP():
+    serverPort = 55556
+    #inet = ipv4 | stream = TCP
+    socketServidor = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+    socketServidor.bind(('', serverPort))
+    #hay qe decirle que empiece a escuchar las conexiones
+    #indica que espere el handshake
+    socketServidor.listen(1) #el parametro indica la cantidad maxima de cola
+    print("Servidor TCP escuchando en puerto: ", serverPort)
+    while True:
+        socketCliente, direccionCliente = socketServidor.accept()
+        mensaje = socketCliente.recv(2048).decode()
+        print("Se recibio: ", mensaje)
+        respuesta = "Respuesta: "+ mensaje.upper()
+        socketCliente.send(respuesta.encode())
+        socketCliente.close()
 
-print("Servidor escuchando en puerto:", serverPort)
+#ServidorUDP()
+#ServidorTCP()
 
-while True:
-    mensaje, direccionCliente = socketServidor.recvfrom(2048)#la cantidad de bits que recive a través del socket - tamaño del buffer
-    decodificado = mensaje.decode()
-    print("Se recibio: ", decodificado)
-    respuesta = "Respuesta: " + decodificado.upper()#dejarlo en mayuscula
-    socketServidor.sendto(respuesta.encode(),direccionCliente)
 
-socketServidor.close()
+
